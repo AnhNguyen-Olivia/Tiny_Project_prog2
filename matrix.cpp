dc16ae65e6b1adbeffc4f718118d8a23c01226ba
ThanhDat4250 <10423136@student.vgu.edu.vn>
2025-06-04 14:46:46 +0000

    ermm

--- matrix.cpp
+++ matrix.cpp
@@ -2,11 +2,11 @@
 #include "matrix.h"
 #include "linearSystem.h"
 
-    // Constructor
-    Matrix::Matrix(int numRows, int numCols) {
-        assert(numRows > 0 && numCols > 0);
-        mNumRows = numRows;
-        mNumCols = numCols;
+// Constructor
+Matrix::Matrix(int numRows, int numCols) {
+    assert(numRows > 0 && numCols > 0);
+    mNumRows = numRows;
+    mNumCols = numCols;
 
     mData = new double*[mNumRows];
     for (int i = 0; i < mNumRows; ++i) {
@@ -47,45 +47,6 @@ Matrix Matrix::PseudoInverse() const {
         Matrix A_T = this->Transpose();
         Matrix ATA = A_T * (*this);
 
-<<<<<<< HEAD
-            // Invert ATA
-            Matrix ATA_inv(mNumCols, mNumCols);
-            for (int i = 1; i <= mNumCols; ++i) {
-                Vector e(mNumCols);
-                e(i) = 1.0;
-                LinearSystem ls(ATA, e);
-                Vector col = ls.Solve();
-                for (int j = 1; j <= mNumCols; ++j) {
-                    ATA_inv(j, i) = col(j);
-                }
-            }
-            return ATA_inv * A_T;
-        } else {
-            // Under-determined: A^T (A A^T)^-1
-            Matrix A_T = this->Transpose();
-            Matrix AAT = (*this) * A_T;
-
-            // Invert AAT
-            Matrix AAT_inv(mNumRows, mNumRows);
-            for (int i = 1; i <= mNumRows; ++i) {
-                Vector e(mNumRows);
-                e(i) = 1.0;
-                LinearSystem ls(AAT, e);
-                Vector col = ls.Solve();
-                for (int j = 1; j <= mNumRows; ++j) {
-                    AAT_inv(j, i) = col(j);
-                }
-            }
-            // *** FIX: Remove .Transpose() ***
-            return A_T * AAT_inv;
-        }
-    }
-
-    // Transpose the matrix
-    Matrix Matrix::Transpose() const { 
-        Matrix result(mNumCols, mNumRows); // Swap rows and columns
-        for (int i = 1; i <= mNumRows; ++i) {
-=======
         // Invert ATA
         Matrix ATA_inv(mNumCols, mNumCols);
         for (int i = 1; i <= mNumCols; ++i) {
@@ -93,7 +54,6 @@ Matrix Matrix::PseudoInverse() const {
             e(i) = 1.0;
             LinearSystem ls(ATA, e);
             Vector col = ls.Solve();
->>>>>>> b6bd0ce7994bdd6ab87a60d6688d44c75335c6f3
             for (int j = 1; j <= mNumCols; ++j) {
                 ATA_inv(j, i) = col(j);
             }
@@ -165,37 +125,37 @@ Matrix& Matrix::operator=(const Matrix& other) {
 int Matrix::GetNumRows() const { return mNumRows; }
 int Matrix::GetNumCols() const { return mNumCols; }
 
-    // Access operator ()
-    double& Matrix::operator()(int i, int j) {
-        assert(i >= 1 && i <= mNumRows && j >= 1 && j <= mNumCols);
-        return mData[i - 1][j - 1];
-    }
+// Access operator ()
+double& Matrix::operator()(int i, int j) {
+    assert(i >= 1 && i <= mNumRows && j >= 1 && j <= mNumCols);
+    return mData[i - 1][j - 1];
+}
 
-    // Const access operator ()
-    const double& Matrix::operator()(int i, int j) const {
-        assert(i >= 1 && i <= mNumRows && j >= 1 && j <= mNumCols);
-        return mData[i - 1][j - 1];
-    }
+// Const access operator ()
+const double& Matrix::operator()(int i, int j) const {
+    assert(i >= 1 && i <= mNumRows && j >= 1 && j <= mNumCols);
+    return mData[i - 1][j - 1];
+}
 
-    // + operator
-    Matrix Matrix::operator+(const Matrix& other) const {
-        assert(mNumRows == other.mNumRows && mNumCols == other.mNumCols);
-        Matrix result(mNumRows, mNumCols);
-        for (int i = 0; i < mNumRows; ++i)
-            for (int j = 0; j < mNumCols; ++j)
-                result.mData[i][j] = mData[i][j] + other.mData[i][j];
-        return result;
-    }
+// + operator
+Matrix Matrix::operator+(const Matrix& other) const {
+    assert(mNumRows == other.mNumRows && mNumCols == other.mNumCols);
+    Matrix result(mNumRows, mNumCols);
+    for (int i = 0; i < mNumRows; ++i)
+        for (int j = 0; j < mNumCols; ++j)
+            result.mData[i][j] = mData[i][j] + other.mData[i][j];
+    return result;
+}
 
-    // - operator
-    Matrix Matrix::operator-(const Matrix& other) const {
-        assert(mNumRows == other.mNumRows && mNumCols == other.mNumCols);
-        Matrix result(mNumRows, mNumCols);
-        for (int i = 0; i < mNumRows; ++i)
-            for (int j = 0; j < mNumCols; ++j)
-                result.mData[i][j] = mData[i][j] - other.mData[i][j];
-        return result;
-    }
+// - operator
+Matrix Matrix::operator-(const Matrix& other) const {
+    assert(mNumRows == other.mNumRows && mNumCols == other.mNumCols);
+    Matrix result(mNumRows, mNumCols);
+    for (int i = 0; i < mNumRows; ++i)
+        for (int j = 0; j < mNumCols; ++j)
+            result.mData[i][j] = mData[i][j] - other.mData[i][j];
+    return result;
+}
 
 // * scalar
 Matrix Matrix::operator*(double scalar) const {
@@ -206,18 +166,6 @@ Matrix Matrix::operator*(double scalar) const {
     return result;
 }
 
-<<<<<<< HEAD
-    // * vector
-    Vector Matrix::operator*(const Vector& vec) const {
-    assert(mNumCols == vec.getSize());
-    Vector result(mNumRows);
-    for (int i = 1; i <= mNumRows; ++i) {
-        result(i) = 0.0;
-        for (int j = 1; j <= mNumCols; ++j)
-            result(i) += (*this)(i, j) * vec(j);
-    }
-    return result;
-=======
 // * vector
 Vector Matrix::operator*(const Vector& vec) const {
     assert(mNumCols == vec.getSize()); 
@@ -226,24 +174,23 @@ Vector Matrix::operator*(const Vector& vec) const {
         result[i] = 0.0;
         for (int j = 0; j < mNumCols; ++j)
             result[i] += mData[i][j] * vec[j];
->>>>>>> b6bd0ce7994bdd6ab87a60d6688d44c75335c6f3
     }
     return result;
 }
 
-    // * matrix
-    Matrix Matrix::operator*(const Matrix& other) const {
-        assert(mNumCols == other.mNumRows);
-        Matrix result(mNumRows, other.mNumCols);
-        for (int i = 0; i < mNumRows; ++i) {
-            for (int j = 0; j < other.mNumCols; ++j) {
-                result.mData[i][j] = 0.0;
-                for (int k = 0; k < mNumCols; ++k)
-                    result.mData[i][j] += mData[i][k] * other.mData[k][j];
-            }
+// * matrix
+Matrix Matrix::operator*(const Matrix& other) const {
+    assert(mNumCols == other.mNumRows);
+    Matrix result(mNumRows, other.mNumCols);
+    for (int i = 0; i < mNumRows; ++i) {
+        for (int j = 0; j < other.mNumCols; ++j) {
+            result.mData[i][j] = 0.0;
+            for (int k = 0; k < mNumCols; ++k)
+                result.mData[i][j] += mData[i][k] * other.mData[k][j];
         }
-        return result;
     }
+    return result;
+}
 
 // Unary minus operator
 Matrix Matrix::operator-() const {
@@ -265,16 +212,16 @@ void Matrix::Print() const {
     }
 }
 
-    double Matrix::Determinant() const {
-        assert(mNumRows == mNumCols);
-        int n = mNumRows;
-        if (n == 1) return mData[0][0];
-        if (n == 2) return mData[0][0] * mData[1][1] - mData[0][1] * mData[1][0];
-        if (n == 3) {
-            return mData[0][0] * (mData[1][1] * mData[2][2] - mData[1][2] * mData[2][1])
-                 - mData[0][1] * (mData[1][0] * mData[2][2] - mData[1][2] * mData[2][0])
-                 + mData[0][2] * (mData[1][0] * mData[2][1] - mData[1][1] * mData[2][0]);
-        }
+double Matrix::Determinant() const {
+    assert(mNumRows == mNumCols);
+    int n = mNumRows;
+    if (n == 1) return mData[0][0];
+    if (n == 2) return mData[0][0] * mData[1][1] - mData[0][1] * mData[1][0];
+    if (n == 3) {
+        return mData[0][0] * (mData[1][1] * mData[2][2] - mData[1][2] * mData[2][1])
+                - mData[0][1] * (mData[1][0] * mData[2][2] - mData[1][2] * mData[2][0])
+                + mData[0][2] * (mData[1][0] * mData[2][1] - mData[1][1] * mData[2][0]);
+    }
 
     double det = 0.0;
     for (int p = 0; p < n; ++p) {
@@ -292,57 +239,16 @@ void Matrix::Print() const {
     return det;
 }
 
-    Matrix Matrix::Inverse() const {
-        assert(mNumRows == mNumCols);
-        int n = mNumRows;
-<<<<<<< HEAD
-        Matrix A(*this);
-        Matrix inv = Matrix::IdentityMatrix(n);
-
-        for (int i = 1; i <= n; ++i) {
-            // Find the pivot
-            double pivot = A(i, i);
-            if (std::abs(pivot) < 1e-12) {
-                // Try to swap with a lower row
-                bool swapped = false;
-                for (int k = i + 1; k <= n; ++k) {
-                    if (std::abs(A(k, i)) > 1e-12) {
-                        // Swap rows i and k in both A and inv
-                        for (int j = 1; j <= n; ++j) {
-                            std::swap(A(i, j), A(k, j));
-                            std::swap(inv(i, j), inv(k, j));
-                        }
-                        pivot = A(i, i);
-                        swapped = true;
-                        break;
-                    }
-                }
-                if (!swapped) throw std::runtime_error("Matrix is singular or nearly singular");
-            }
-            // Normalize the pivot row
-            for (int j = 1; j <= n; ++j) {
-                A(i, j) /= pivot;
-                inv(i, j) /= pivot;
-            }
-            // Eliminate other rows
-            for (int k = 1; k <= n; ++k) {
-                if (k == i) continue;
-                double factor = A(k, i);
-                for (int j = 1; j <= n; ++j) {
-                    A(k, j) -= factor * A(i, j);
-                    inv(k, j) -= factor * inv(i, j);
-                }
-            }
-        }
-=======
-        double det = this->Determinant();
-        const double EPS = 1e-12;
-        assert(std::abs(det) > EPS && "Matrix is singular or nearly singular");
+Matrix Matrix::Inverse() const {
+    assert(mNumRows == mNumCols);
+    int n = mNumRows;
+    double det = this->Determinant();
+    const double EPS = 1e-12;
+    assert(std::abs(det) > EPS && "Matrix is singular or nearly singular");
 
     Matrix inv(n, n);
     if (n == 1) {
         inv.mData[0][0] = 1.0 / mData[0][0];
->>>>>>> b6bd0ce7994bdd6ab87a60d6688d44c75335c6f3
         return inv;
     }
     if (n == 2) {
